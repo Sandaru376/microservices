8e6ac0e84742a490409da43aee9aef1ae7366acd
Sandaru <sandaru376@gmail.com>
2026-02-12 18:44:33 +0530

    Implemented microservice communication using WebClient

--- department-service/src/main/java/com/springbootacademy/department_service/service/impl/DepartmentServiceIMPL.java
+++ department-service/src/main/java/com/springbootacademy/department_service/service/impl/DepartmentServiceIMPL.java
@@ -17,37 +17,37 @@ public class DepartmentServiceIMPL implements DepartmentService {
 //        this.departmentRepository = departmentRepository;
 //    }
 
-//    @Override
-//    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
-//        Department department = new Department(
-//                departmentDto.getId(),
-//                departmentDto.getDepartmentName(),
-//                departmentDto.getDepartmentDescription(),
-//                departmentDto   .getDepartmentCode()
-//        );
-//
-//        Department savedepartment1=departmentRepository.save(department);
-//
-//        DepartmentDto SaveddepartmentDto=new DepartmentDto(
-//                savedepartment1.getId(),
-//                savedepartment1.getDepartmentName(),
-//                savedepartment1.getDepartmentDescription(),
-//                savedepartment1.getDepartmentCode()
-//        );
-//
-//        return SaveddepartmentDto;
-//    }
-//
-//    @Override
-//    public DepartmentDto getDepartmentByCode(String code) {
-//        Department department=departmentRepository.findByDepartmentCode(code);
-//        DepartmentDto departmentDto=new DepartmentDto(
-//                department.getId(),
-//                department.getDepartmentName(),
-//                department.getDepartmentDescription(),
-//                department.getDepartmentCode()
-//        );
-//
-//        return departmentDto;
-//    }
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto   .getDepartmentCode()
+        );
+
+        Department savedepartment1=departmentRepository.save(department);
+
+        DepartmentDto SaveddepartmentDto=new DepartmentDto(
+                savedepartment1.getId(),
+                savedepartment1.getDepartmentName(),
+                savedepartment1.getDepartmentDescription(),
+                savedepartment1.getDepartmentCode()
+        );
+
+        return SaveddepartmentDto;
+    }
+
+    @Override
+    public DepartmentDto getDepartmentByCode(String code) {
+        Department department=departmentRepository.findByDepartmentCode(code);
+        DepartmentDto departmentDto=new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription(),
+                department.getDepartmentCode()
+        );
+
+        return departmentDto;
+    }
 }
